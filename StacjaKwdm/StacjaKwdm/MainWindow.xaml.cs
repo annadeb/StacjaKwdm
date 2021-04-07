a7f0c278154af85c687b1f5ec4c058b4ec7c7384
Anna Dębowska <anna-debowska@o2.pl>
2021-04-07 19:59:25 +0200

    Wybór i przechwycenie punktu startowego

--- StacjaKwdm/StacjaKwdm/MainWindow.xaml.cs
+++ StacjaKwdm/StacjaKwdm/MainWindow.xaml.cs
@@ -27,6 +27,9 @@ using StacjaKwdm.Models;
 //using Aspose.Imaging.Sources;
 using Dicom;
 using Dicom.Imaging;
+using segmentation;
+using MathWorks.MATLAB.NET.Arrays;
+using ColorPickerControls;
 
 namespace StacjaKwdm
 {
@@ -39,6 +42,8 @@ namespace StacjaKwdm
 		public RestClient _client;
 		public int sliderValue;
 		public string seriesUID;
+		public System.Windows.Point _position;
+
 		public MainWindow()
 		{
 			InitializeComponent();
@@ -89,11 +94,18 @@ namespace StacjaKwdm
 
 				var request2 = new RestRequest("instances/" + item + "/file", Method.GET); // /preview do .png
 				request2.AddHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
-				var query2 = _client.Execute(request2);
+				//var query2 = _client.Execute(request2);
 				_client.DownloadData(request2).SaveAs(seriesUID+ "/" + item + ".dcm"); //asd.png
 			}
 			pictureSlider.Maximum = instances.Count() - 1;
 			DisplayDicom(seriesUID, sliderValue);
+			//string[] filesInDirectory = Directory.GetFiles(seriesUID).ToArray();
+			//for (int i = 0; i < filesInDirectory.Length; i++)
+			//{
+			//	string path = filesInDirectory[i];
+			//	var dicomImg = new DicomImage(@path);
+			//	//var cos = dicomImg.Dataset.GetSequence(new DicomTag(0010, 0010));
+			//}
 		}
 
 		private void pictureSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
@@ -115,6 +127,21 @@ namespace StacjaKwdm
 			BitmapSizeOptions.FromWidthAndHeight(renderedImage.Width, renderedImage.Height));
 			image1.Source = ScreenCapture;
 
+		}
+
+		private void autoSegmentButton_Click(object sender, RoutedEventArgs e)
+		{
+			_position = image1.Position;
+			if (_position.X==0 && _position.Y==0)
+			{
+				MessageBox.Show("Wybierz punkt startowy!");
+				return;
+			}
+			
+			//var klasa = new Class1();
+			//MWArray folderpath = "C:\\Users\\Anna\\Desktop\\Studia\\MAGISTERKA_3\\KomputeroweWspomaganieDiagnostykiMedycznej\\Projekt\\StacjaKwdm\\StacjaKwdm\\StacjaKwdm\\bin\\Debug\\c71658e3-68b7c35c-5216242c-fb200b08-aa56b7d0";
+			//var cos = klasa.segmentation(folderpath);
+			int a = 0;
 		}
 	}
 }
